fb90e937c727b5bc1f457229e01c24214fdb8813
sichallez <sich.scidonnees@gmail.com>
2022-01-25 18:31:51 -0500

    first submission

--- acme_employees.js
+++ acme_employees.js
@@ -21,11 +21,23 @@ const employees = [
   // given a name and array of employees, return employee
   console.log(findEmployeeByName('moe', employees));//{ id: 1, name: 'moe' }
   spacer('')
+
+  function findEmployeeByName(name, employeeArray) {
+    const employeeArray.filter(element => element.name === name);
+    return employee[0];
+  }
   
   spacer('findManagerFor Shep Jr.')
   //given an employee and a list of employees, return the employee who is the manager
   console.log(findManagerFor(findEmployeeByName('shep Jr.', employees), employees));//{ id: 4, name: 'shep', managerId: 2 }
   spacer('')
+
+  function findManagerFor(func, employeeArray) {
+    const employee = func;
+    const managerId = employee.managerId;
+    const manager = employeeArray.filter(element => element.id === managerId);
+    return manager[0];
+  }
   
   spacer('findCoworkersFor Larry')
   
@@ -36,6 +48,12 @@ const employees = [
   */
   
   spacer('');
+
+  function findCoworkersFor(func, employeeArray) {
+    const employee = func;
+    const managerId = employee.managerId;
+    return coWorker = employeeArray.filter(element => element.managerId === managerId && element.id !== employee.id);
+  }
   
   spacer('findManagementChain for moe')
   //given an employee and a list of employees, return a the management chain for that employee. The management chain starts from the employee with no manager with the passed in employees manager 
@@ -50,6 +68,16 @@ const employees = [
   */
   spacer('');
   
+  function findManagementChainForEmployee(func, employeeArray) {
+    const employee = func;
+    let chain = [];
+    const manager = findManagerFor(employee, employeeArray);
+    if (employee.managerId === undefined) return [];
+    else {
+      chain = findManagementChainForEmployee(manager, employeeArray).concat([manager]);
+    }
+    return chain;
+  }
   
   spacer('generateManagementTree')
   //given a list of employees, generate a tree like structure for the employees, starting with the employee who has no manager. Each employee will have a reports property which is an array of the employees who report directly to them.
@@ -109,6 +137,23 @@ const employees = [
   }
   */
   spacer('');
+
+  function generateManagementTree(employeeArray) {
+    let level1 = employeeArray.filter(element => element.managerId === undefined);
+    level1 = level1[0];
+    const level2 = employeeArray.filter(element => element.managerId === level1.id);
+    level2.forEach(element => {
+      let level3 = employeeArray.filter(elem => elem.managerId === element.id);
+      level3.forEach(element => {
+        let level4 = employeeArray.filter(elem => elem.managerId === element.id);
+        level4[0]['report'] = [];
+        element['report'] = level4;
+      });
+      element['report'] = level3;
+    });
+    level1['report'] = level2;  
+    return level1;
+  }
   
   spacer('displayManagementTree')
   //given a tree of employees, generate a display which displays the hierarchy
